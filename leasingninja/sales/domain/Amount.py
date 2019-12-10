55f8487d9f0ff7112c0b8a26319d081418663b1e
Henning Schwentner <hs@wps.de>
2019-12-10 18:17:04 +0100

    Override __eq__

--- leasingninja/sales/domain/Amount.py
+++ leasingninja/sales/domain/Amount.py
@@ -9,7 +9,7 @@ class Amount:
     def currency(self) -> str:
         return self._currency
 
-    def __eq__(self, other):
+    def __eq__(self, other) -> bool:
         return (
             self.__class__ == other.__class__ and
             self._amount == other._amount and
