fe948d2d8a69134e9fc28d3e285f12b6814bcb5a
tbeu <tc@tbeu.de>
2025-02-07 19:13:09 +0100

    Fix issues reported by flake8

--- tests/leasingninja/sales/domain/testAmount.py
+++ tests/leasingninja/sales/domain/testAmount.py
@@ -10,11 +10,11 @@ class AmountTest(unittest.TestCase):
         amount2: Amount = Amount(100, "EUR")
 
         # when
-        #are_equal: bool = amount1.equals(amount2)
+        # are_equal: bool = amount1.equals(amount2)
         are_equal: bool = amount1 == amount2
 
         # then
-        #assertThat(areEqual).isTrue();
+        # assertThat(areEqual).isTrue();
         self.assertTrue(are_equal)
 
     def test_givenTwoUnequalAmounts_whenEquals_thenAreNotEqual(self):
